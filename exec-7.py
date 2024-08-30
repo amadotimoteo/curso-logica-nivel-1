9cd30bfbb2a9ac885eb9862b909bfd5efc559496
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 20:54:36 -0300

    Create exec-7.py

---
+++ exec-7.py
@@ -0,0 +1,13 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+#Enriqueça o desenho com outros caracteres, simulando enfeites.
+print("       &)")
+print("      X%X)")
+print("     #X#X#)")
+print("    X!X!X!X)")
+print("   X*X*X*X*X)")
+print("  X=X=X=X=X=)")
+print(" %X%X%X%X%X%X%)")
+print("X$X$X$X$X$X$X$X)")
+print("       XX)")
+print("       XX)")
+print("      XXXX)")
