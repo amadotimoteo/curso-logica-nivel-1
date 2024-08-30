9a91b165f334da4848122307ac115dc0f40bf4e5
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 20:14:05 -0300

    Create  exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,4 @@
+#3. Letra de música - Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+print("Artista: Sondae")
+print("Musica: Miracle")
+print("letra: Sanctified because You chose to take my cross")
