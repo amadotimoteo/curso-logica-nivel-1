4bacb6a0c0c9c8081d9b0ccf0527889d89bdd4c1
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 19:51:08 -0300

    Update exec-1.py

--- exec-1.py
+++ exec-1.py
@@ -1,4 +1,2 @@
-
-#1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
-
+#1. Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".
 print("o primeiro programa a gente nunca esquece")
