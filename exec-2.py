b8a8ff022c5764c541cd9e817ae690daeccbd587
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 20:07:03 -0300

    Create exec-2.py

---
+++ exec-2.py
@@ -0,0 +1,4 @@
+#2. Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Nome: Gabriel Alves")
+print ("Cidade: Rio de Janeiro")
+print("Municipio: Rio de Janeiro")
