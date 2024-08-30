001bedfa6747727632eba7bd63101e3bc63e4dc4
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 20:36:12 -0300

    Create exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,8 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela: Cadastro de Clientes
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+print("")
+print("Opção:")
