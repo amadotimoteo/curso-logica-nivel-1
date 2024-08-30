b099fcb148b0243d7f104c47ac86c4bbe808f3f5
amadotimoteo <oamadotimoteo@icloud.com>
2024-08-30 20:23:29 -0300

    Create exec-5.py

---
+++ exec-5.py
@@ -0,0 +1,7 @@
+#5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+print("ALUNO (a)   NOTA")
+print("=========   ====")
+print("ALINE        9.0")
+print("MÁRIO        DEZ")
+print("SÉRGIO       4.5")
+print("SHIRLEY      7.0")
